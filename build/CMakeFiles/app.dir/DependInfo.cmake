3b623f97c28defdcae58b0f8d6e5f3744df1c07e
abence1 <acsbence25@gmail.com>
2025-05-14 22:50:00 +0200

    object class implementation

---
+++ build/CMakeFiles/app.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/acsbence/Documents/C++/2drender/OpenGL_2DRenderer/src/glad.c" "CMakeFiles/app.dir/src/glad.c.o" "gcc" "CMakeFiles/app.dir/src/glad.c.o.d"
+  "/Users/acsbence/Documents/C++/2drender/OpenGL_2DRenderer/src/main.cpp" "CMakeFiles/app.dir/src/main.cpp.o" "gcc" "CMakeFiles/app.dir/src/main.cpp.o.d"
+  "/Users/acsbence/Documents/C++/2drender/OpenGL_2DRenderer/src/object.cpp" "CMakeFiles/app.dir/src/object.cpp.o" "gcc" "CMakeFiles/app.dir/src/object.cpp.o.d"
+  "/Users/acsbence/Documents/C++/2drender/OpenGL_2DRenderer/src/shader.cpp" "CMakeFiles/app.dir/src/shader.cpp.o" "gcc" "CMakeFiles/app.dir/src/shader.cpp.o.d"
+  "/Users/acsbence/Documents/C++/2drender/OpenGL_2DRenderer/src/vao.cpp" "CMakeFiles/app.dir/src/vao.cpp.o" "gcc" "CMakeFiles/app.dir/src/vao.cpp.o.d"
+  "/Users/acsbence/Documents/C++/2drender/OpenGL_2DRenderer/src/window.cpp" "CMakeFiles/app.dir/src/window.cpp.o" "gcc" "CMakeFiles/app.dir/src/window.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
